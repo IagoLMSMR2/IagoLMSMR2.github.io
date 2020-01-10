2f789c42f65db8cfa759668c3055f5499d262009
IagoLMSMR2 <56600595+IagoLMSMR2@users.noreply.github.com>
2020-01-10 11:47:37 +0100

    Add files via upload

---
+++ tarefa3/escripito.js
@@ -0,0 +1,16 @@
+var numero1 = 1.5684 ;
+var numero2 = 2.5648 ;
+var numero3 = 3.6547 ;
+
+var raiz = Math.sqrt(numero1);
+var cuadrado = Math.pow(numero2, 2);
+var pi = Math.PI;
+var numeroPi = Math.round(pi);
+var numeroMasPequeño = Math.min(numero1, numero2, numero3);
+var random = Math.random(1, 3); 
+
+alert(raiz);
+alert(cuadrado);
+alert(numeroPi);
+alert(numeroMasPequeño);
+alert(random);
